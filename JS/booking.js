d8a58df64e21da0e2bea4389b6d3422dee9531a2
Awab-Elsadig <117605732+Awab-Elsadig@users.noreply.github.com>
2023-07-22 06:53:18 +0300

    update 7 files

--- JS/booking.js
+++ JS/booking.js
@@ -1,4 +1,23 @@
-function toggleSeats(room) {
+const rooms = document.querySelectorAll(".room");
+
+rooms.forEach((room) => {
    const seats = room.querySelector(".seats");
-   seats.classList.toggle("hidden");
-}
+
+   room.addEventListener("click", () => {
+      seats.classList.toggle("hidden");
+      if (!seats.classList.contains("hidden")) {
+         seats.style.maxHeight = `${seats.scrollHeight}px`;
+      } else {
+         seats.style.maxHeight = "0";
+      }
+   });
+
+   // Prevent click event propagation from seats to room
+   const roomSeats = room.querySelectorAll(".seat");
+   roomSeats.forEach((seat) => {
+      seat.addEventListener("click", (event) => {
+         event.stopPropagation();
+         seat.classList.toggle("booked");
+      });
+   });
+});
