12990dc36efe04811aca1b134b0af1548081632d
Awab-Elsadig <117605732+Awab-Elsadig@users.noreply.github.com>
2023-07-25 06:41:16 +0300

    update main.css, booking.js, _booking.scss and booking.html

--- JS/booking.js
+++ JS/booking.js
@@ -1,59 +1,3 @@
-// let rooms = document.querySelectorAll(".room");
-
-// // Loop on each room
-// rooms.forEach((room) => {
-//    room.addEventListener("click", () => {
-//       // Expand the toggled room and remover toggle from any other room
-//       rooms.forEach((otherRoom) => {
-//          if (otherRoom !== room) {
-//             otherRoom.classList.remove("expanded");
-//             otherRoom.querySelector(".seats").classList.add("hidden");
-//          }
-//       });
-
-//       let seatsElement = room.querySelector(".seats");
-//       room.classList.toggle("expanded");
-//       seatsElement.classList.toggle("hidden");
-
-//       rooms.forEach((room) => {
-//          let seatsElement = room.querySelector(".seats");
-//          if (room.classList.contains("expanded")) {
-//             seatsElement.style.maxHeight = `${seatsElement.scrollHeight}px`;
-//          } else {
-//             seatsElement.style.maxHeight = 0;
-//          }
-//       });
-
-//       // Change Seat to booked
-//       let seats = seatsElement.querySelectorAll(".seat");
-//       seats.forEach((seat) => {
-//          seat.addEventListener("click", (event) => {
-//             event.stopPropagation();
-//             if (!(seat.classList.contains === "reserved")) {
-//                seat.classList.toggle("booked");
-//                updateSelection();
-//                updatePrice();
-//             }
-//          });
-//       });
-
-//       function updateSelection() {
-//          let selection = document.querySelector(".selected");
-//          let bookedSeatsOld = document.querySelectorAll(".booked");
-//          let bookedSeats = new Set();
-//          bookedSeatsOld.forEach((seat) => {
-//             let theSeatP = document.createElement("p");
-//             theSeatP.textContent = `${seat.parentElement.parentElement.querySelector("h2").textContent} - ${seat.textContent}`;
-//             bookedSeats.add(theSeatP);
-//          });
-//          selection.textContent = "";
-//          bookedSeats.forEach((seat) => selection.append(seat));
-//          // bookedSeats.forEach((seat) => selection.append(seat.textContent));
-//          document.querySelector(".total__price span").textContent = `${bookedSeats.size * 30} EGP`;
-//       }
-//    });
-// });
-
 // Function to expand or collapse a room
 function toggleRoom(room) {
    rooms.forEach((otherRoom) => {
@@ -77,7 +21,7 @@ function toggleRoom(room) {
    });
 }
 
-// Function to change seat status to booked
+// Function to change seat status to booked and update selction
 function toggleSeatBooking(seat) {
    if (!seat.classList.contains("reserved")) {
       seat.classList.toggle("booked");
@@ -92,16 +36,29 @@ function updateSelection() {
    let bookedSeats = new Set();
 
    bookedSeatsOld.forEach((seat) => {
-      let theSeatP = document.createElement("p");
-      theSeatP.textContent = `${seat.parentElement.parentElement.querySelector("h2").textContent} - ${seat.textContent}`;
-      bookedSeats.add(theSeatP);
+      let seatDiv = document.createElement("div");
+      seatDiv.className = "seatInSelected";
+      let seatRoom = document.createElement("div");
+      seatRoom.className = "seatRoom";
+      seatRoom.textContent = `${seat.parentElement.parentElement.querySelector("h2").textContent} - `;
+      let seatNumber = document.createElement("div");
+      seatNumber.className = "seatNumber";
+      seatNumber.textContent = seat.textContent;
+      seatDiv.append(seatRoom, seatNumber);
+      bookedSeats.add(seatDiv);
    });
 
    selection.textContent = "";
-   bookedSeats.forEach((seat) => selection.append(seat));
+   bookedSeats.forEach((seat) => {
+      selection.append(seat);
+   });
+
+   bookedSeats.forEach((seat) => {
+      sessionStorage.setItem(seat, JSON.stringify(seat));
+   });
 
    document.querySelector(".total__price span").textContent = `${bookedSeats.size * 30} EGP`;
-   bookedSeats.size > 0 ? document.querySelector(".checkout__button").style.display = "flex" : document.querySelector(".checkout__button").style.display = "none";
+   bookedSeats.size > 0 ? (document.querySelector(".checkout__button").style.display = "flex") : (document.querySelector(".checkout__button").style.display = "none");
 }
 
 // Add event listener to each room
